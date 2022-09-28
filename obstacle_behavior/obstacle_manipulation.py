05f0d6df28fff59104fa79fe51240a7aea443242
mateusb12 <mateus_b09@hotmail.com>
2022-09-28 11:38:06 -0300

    dummy json

--- obstacle_behavior/obstacle_manipulation.py
+++ obstacle_behavior/obstacle_manipulation.py
@@ -1,9 +1,14 @@
+from pathlib import Path
+
 import pygame
 
 from agent_behavior.corner_object import Corner
 from agent_behavior.polygon_obstacle import PolygonObstacle
 from mouse_behavior.cursor_changing import CursorBehavior
 from agent_behavior.tools import rectangle_parameters_from_coordinates
+import json
+
+from references import get_obstacle_behavior_folder
 
 
 class ObstacleManipulation:
@@ -26,18 +31,19 @@ class ObstacleManipulation:
         return pygame.Rect(rectangle_params)
 
     def create_dummy_polygon(self):
-        polygon_points = [(259, 639), (258, 788), (576, 795), (574, 771), (496, 740), (496, 639)]
-        full_points = [
-            [(259, 639), (258, 788), (576, 795), (574, 771), (496, 740), (496, 639)],
-            [(545, 626), (699, 626), (699, 676), (545, 676)],
-            [(582, 516), (581, 526), (607, 527), (606, 569), (652, 569), (654, 593), (743, 595), (742, 572), (766, 570),
-             (763, 546), (624, 545), (625, 517), (585, 517)]
-        ]
+        # polygon_points = [(259, 639), (258, 788), (576, 795), (574, 771), (496, 740), (496, 639)] full_points = [ [
+        # (259, 639), (258, 788), (576, 795), (574, 771), (496, 740), (496, 639)], [(545, 626), (699, 626), (699,
+        # 676), (545, 676)], [(582, 516), (581, 526), (607, 527), (606, 569), (652, 569), (654, 593), (743, 595),
+        # (742, 572), (766, 570), (763, 546), (624, 545), (625, 517), (585, 517)] ]
+        obstacle_list_path = Path(get_obstacle_behavior_folder(), "obstacle_point_list.json")
+
+        with open(obstacle_list_path, "r") as file:
+            full_points = json.load(file)["lists"]
         for point_list in full_points:
             self.create_obstacle_from_clicks(point_list)
 
     def create_dummy_corner(self):
-        corner_points = (468, 625)
+        corner_points = (834, 490)
         new_corner = Corner(x=corner_points[0], y=corner_points[1], screen=self.screen)
         self.corner_pool.append(new_corner)
 
@@ -54,7 +60,8 @@ class ObstacleManipulation:
     def rectangle_creator_from_clicks(self):
         self.clicks += 1
         print(self.clicks)
-        self.click_positions.append(pygame.mouse.get_pos())
+        pos = list(pygame.mouse.get_pos())
+        self.click_positions.append(pos)
 
     def rectangle_finish_creation(self):
         self.create_obstacle_from_clicks(self.click_positions)
--- references.py
+++ references.py
@@ -3,3 +3,7 @@ from pathlib import Path
 
 def get_assets_folder():
     return Path(Path(__file__).parent / "assets")
+
+
+def get_obstacle_behavior_folder():
+    return Path(Path(__file__).parent / "obstacle_behavior")
