ad0d845af59f93ce5d99c39d79ae51ef1fe8ac34
mateusb12 <mateus_b09@hotmail.com>
2022-09-29 16:02:02 -0300

    real coordinates

--- agent_behavior/tools.py
+++ agent_behavior/tools.py
@@ -4,3 +4,7 @@ def rectangle_parameters_from_coordinates(top_left: tuple[int, int], top_right: 
     width = top_right[0] - top_left[0]
     height = bottom_left[1] - top_left[1]
     return top_left[0], top_left[1], width, height
+
+
+def convert_real_coordinates_to_pygame_coordinates(real_x: int, real_y: int):
+    return real_x - 317, real_y + 78
